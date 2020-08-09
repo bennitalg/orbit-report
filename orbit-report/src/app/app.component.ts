e6ba561c544740d44e061068f8155cdd46ad602c
bennitalg <40967401+bennitalg@users.noreply.github.com>
2020-08-09 16:18:57 -0500

    Assignment 6

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Satellite } from './satellite';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];//an array of objects called sourceList
@@ -25,9 +26,26 @@ export class AppComponent {
          let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
          this.sourceList.push(satellite);
          }
+        this.displayList = this.sourceList.slice(0);
+
       }.bind(this));
    }.bind(this));
 
   }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    // assign this.displayList to be the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
+    
+    } 
  }
 
